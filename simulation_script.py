bcdf1b1c826df34a18d27846c73b3093966a7775
altug <altugkarakurt@gmail.com>
2015-09-08 11:45:40 +0300

    Updated simulation script for multiple servers

--- simulation_script.py
+++ simulation_script.py
@@ -5,8 +5,10 @@ import Server
 simulator = Simulator.Simulator(run_time=1000, package_limit=20,
 	                            scheduling_type='shortest_queue')
 
-server = Server.Server(server_id=1, simulator=simulator, capacity=5)
-simulator.add_resource(server)
+server1 = Server.Server(server_id=1, simulator=simulator, capacity=5)
+server2 = Server.Server(server_id=2, simulator=simulator, capacity=5)
+simulator.add_resource(server1)
+simulator.add_resource(server2)
 
 pg = PackageGenerator.PackageGenerator(lambd=5, process_time=20,
 									   simulator=simulator, 
