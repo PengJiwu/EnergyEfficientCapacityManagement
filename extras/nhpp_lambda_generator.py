d3df7ad012a3c07456970a9fa623222aa6325efc
altug <altugkarakurt@gmail.com>
2015-11-02 23:23:08 +0300

    Added and modified traces

--- extras/nhpp_lambda_generator.py
+++ extras/nhpp_lambda_generator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as pl
+from random import randint
 
 """--------------------------------------------------------------------
 This is an example script to extract time dependent lambda from actual
@@ -7,20 +9,17 @@ data.
 LIMITATION: Here we are modelling a Non-staionary Poisson Process,
 which implies that requests must arrive one-at-a-time. Multiple
 requests can't arrive at the same time. This is a commonly used
-approach, but be aware of this limitation, since it might be
-problematic depending on your task.
+approach, but be aware of this limitation.
 --------------------------------------------------------------------"""
-
 INTERVAL_LEN = 50
-LAMBDA_LEN = 10000
+LAMBDA_LEN = 5000
 
-lambd = [0 for _ in xrange(LAMBDA_LEN)]
+# Generation of Quickly Varying Trace
+lambd = np.zeros(LAMBDA_LEN)
 
 for n, _ in enumerate(lambd):
-	lambd[n] = 400 * (np.sin(n / (800/(2*np.pi))) + 1)
+	lambd[n] = 295 * ((np.sin(n / (16000/(2*np.pi))) + 1) + 0.7 * np.sin(n / (750/(2*np.pi))))
 
-# pl.plot(lambd)
-# pl.show()
 
 avg_vals = []
 avg_bins = []
@@ -37,6 +36,9 @@ for i in xrange((LAMBDA_LEN/INTERVAL_LEN) + 1):
 		avg_bins.append(cur_lim)
 		avg_vals.append(avg)
 
-with open('./nhpp_data/sinusoid.txt', 'w') as f:
+pl.stem(avg_bins, avg_vals)
+pl.show()
+
+with open('./quickly_varying.txt', 'w') as f:
 	for bn, val in zip(avg_bins, avg_vals):
 		f.write(str(bn) + '\t' + str(val) + '\n')
